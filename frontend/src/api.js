9cc261f5878e6c24f8dc904810acf475b2099a52
Siddharth Ranka <siddharthranka91@gmail.com>
2025-12-30 23:20:54 +0530

    fixed vercel issue

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // Hardcode the link directly to bypass the Vercel variable issue for now
-    // baseURL: "https://intern-track-backend-new.onrender.com/api",
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://intern-track-backend-new.onrender.com/api",
     withCredentials: true // MUST BE TRUE
 });
 
